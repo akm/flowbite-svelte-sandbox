bda31d49acf6554ce01283cf18a77a8d3b7bd599
akm <akm2000@gmail.com>
2023-11-04 14:25:35 +0900

    :sparkles: 画面の表記を日本語に

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -4,25 +4,25 @@
 
 <div class="container mx-auto px-8 py-4">
 	<form class="flex flex-col space-y-6" action="#">
-		<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
+		<h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">ログイン</h3>
 		<Label class="space-y-2">
-			<span>Email</span>
+			<span>メールアドレス</span>
 			<Input type="email" name="email" placeholder="name@company.com" required />
 		</Label>
 		<Label class="space-y-2">
-			<span>Your password</span>
+			<span>パスワード</span>
 			<Input type="password" name="password" placeholder="•••••" required />
 		</Label>
 		<div class="flex items-start">
-			<Checkbox>Remember me</Checkbox>
+			<Checkbox>ログイン状態を記録します</Checkbox>
 			<a href="/" class="ml-auto text-sm text-primary-700 hover:underline dark:text-primary-500">
-				Lost password?
+				パスワードを忘れた場合
 			</a>
 		</div>
-		<Button type="submit" class="w-full1">Login to your account</Button>
+		<Button type="submit" class="w-full1">ログイン</Button>
 		<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-			Not registered? <a href="/" class="text-primary-700 hover:underline dark:text-primary-500">
-				Create account
+			まだ未登録ですか？ <a href="/" class="text-primary-700 hover:underline dark:text-primary-500">
+				アカウントの作成
 			</a>
 		</div>
 	</form>
